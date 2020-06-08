54d4fc1d86c9925e38cdf127f9ddd7ee6228f8e6
RuslanZaremba <ruslanzaremba1990@gmail.com>
2020-06-08 18:46:33 +0300

    factorial

--- day1/factorial.py
+++ day1/factorial.py
@@ -1,13 +1,8 @@
 # Считаем факториал
 i = int(input('Введите число для вычисления факториала: '))
-if i == 0:
-    print(i, '!=', 1)
-    quit()
-
 a = i
 factorial = 1
 while i > 1:
     factorial *= i
     i -= 1
 print(a, '!=', factorial)
-
