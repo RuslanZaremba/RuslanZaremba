55c297275859699c38b2485b2be3f722380acf41
RuslanZaremba <ruslanzaremba1990@gmail.com>
2020-06-15 14:37:37 +0300

    сделал 11 задачу

--- homework7/task10.py
+++ homework7/task10.py
@@ -1,7 +1,18 @@
 # 10. Создать декоратор для функции, которая принимает список чисел.
 # Декоратор должен производить предварительную проверку данных -
 # удалять все четные элементы из списка.
-def decor(func):
-    def wrapper():
-        a = list(func())
-        a = list(map(lambda x: not x % 2),a)
+def decorator(func):
+    def wrapper(*args):
+        fu = func(*args)
+        result = list(filter(lambda x: x % 2, fu))
+        return print(result)
+
+    return wrapper
+
+
+@decorator
+def nums(num):
+    return num
+
+
+print(nums([1, 2, 3, 4, 5, 6, 7]))
--- homework7/task9.py
+++ homework7/task9.py
@@ -1,6 +1,6 @@
 # Создать lambda функцию, которая принимает на вход неопределенное количество
 # именных аргументов и выводит словарь с ключами удвоенной длины. {‘abc’: 5} -> {‘abcabc’: 5}
-di = {'a': 2,'b':3}
+di = {'a': 2, 'b': 3}
 # func = [(lambda i, y: {i * 2: y})(key, value) for key, value in di.items()]
 for key, value in di.items():
     print((lambda i, y: {i * 2: y})(key, value))
