0f84d66f307be4bf2ad7518f4ffd4eabc952211e
RuslanZaremba <ruslanzaremba1990@gmail.com>
2020-06-15 11:52:13 +0300

    задание 10

--- homework7/task8.py
+++ homework7/task8.py
@@ -1,3 +1,4 @@
 lines = ['asd', 'wqe', 'aasfq']
-a = [(lambda i: '{}-{}'.format(i, a[i]))(i) for i in range(len(a))]
-print(a)
+for i in range(len(lines)):
+    s = list(map(lambda i: '{} - {}'.format(i, lines[i]), lines))
+    print(s)
