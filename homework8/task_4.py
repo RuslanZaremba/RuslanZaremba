7bd52d1cca05f9e28f27f3b62b474deec1f90a27
RuslanZaremba <ruslanzaremba1990@gmail.com>
2020-06-18 15:57:18 +0300

    Разобрался с 7 задачей

--- homework8/task_4.py
+++ homework8/task_4.py
@@ -1,31 +1,6 @@
-# with open('task4.txt') as file:
-#     with open('task4_new.txt', 'w') as new_file:
-<<<<<<< HEAD
-#         new_file.write(file.read())
-#         print(type(file.read()))
-# a = '10101'
-# print(a)
-# b = list(map(lambda x: 1 if int(x) == 0 else 0, a))
-# print(''.join(b))
 with open('text_files/task4.txt') as file:
-    with open('text_files/task4_new.txt', 'w') as new_file:
-        for line in file:
-            for i in line:
-                if i.isdigit():
-                    i = map(lambda x: 1 if int(x) == 0 else 0)
-            # new_list = list(map(lambda x: 1 if int(x) == 0 else 0, line))
-            # new_row = ''.join(map(lambda x: str(x)
-            # new_file.write(new_list)
-=======
-#         for i in file.read():
-#             if i.isdigit():
-#                 b = list(map(lambda x: 1 if int(x) == 0 else 0, i))
-#                 new_file.write(''.join(map(str, b)))
-#             else:
-#                 continue
+    file = file.read()
 
-with open('task4.txt') as file:
-    with open('task4_new.txt', 'w') as new_file:
-        for i in range(len(file.read())-1):
-            print(i)
-
+with open('text_files/task4_new.txt', 'w') as new_file:
+    changed_0_in_file = file.replace('0', '{nul}').replace('1', '0').replace('{nul}', '1')
+    new_file.write(changed_0_in_file)
--- homework8/task_7.py
+++ homework8/task_7.py
@@ -1,17 +1,25 @@
 import random
 import json
-import pprint
-# matrix = [[random.randint(1, 10) for i in range(0, 5)] for j in range(0, 5)]
-# with open('text_files/matrix.json', 'w') as file:
-#     file.write(json.dumps(matrix))
-# with open('text_files/matrix.json', 'r') as file:
-#     with open('text_files/new_matrix.json', 'w') as new_file:
-#         my_matrix = json.loads(file.read())
-#         for i in range(len(my_matrix)):
-#             for j in range(len(my_matrix[i])):
-#                 if my_matrix[i][j] % 2 == 0:
-#                     my_matrix[i][j] = 0
-#         new_file.write(json.dumps(my_matrix))
+
+
+def saver(matrix, file):
+    with open(file, 'w') as file:
+        data = {'matrix': matrix}
+        file.write(json.dumps(data))
+
+
+matrix = [[random.randint(1, 10) for i in range(0, 5)] for j in range(0, 5)]
+
+saver(matrix, 'text_files/matrix.json')
+
+with open('text_files/matrix.json') as file:
+    matrix_unpacked = json.loads(file.read())['matrix']
+for i in range(len(matrix_unpacked)):
+    for j in range(len(matrix_unpacked[i])):
+        if matrix_unpacked[i][j] % 2 == 0:
+            matrix_unpacked[i][j] = 0
+saver(matrix_unpacked, 'text_files/new_matrix.json')
+
 
 # def saver(matrix, file):
 #     with open(file, 'w') as f:
@@ -33,12 +41,3 @@ import pprint
 #
 # saver(matrix, 'matrix.json')
 
-data = {}
-with open('big_data.json', 'r', encoding="UTF-8") as file:
-    data = json.loads(file.read())
-    pprint.pprint(data)
-    print(data)
-
-
-with open('big_data.json', 'w', encoding="UTF-8") as file:
-    data = json.loads(file.read())
