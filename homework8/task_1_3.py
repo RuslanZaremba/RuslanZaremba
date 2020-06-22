d9514cedefbdbf60ebb2d9bcb8808a593722f3eb
RuslanZaremba <ruslanzaremba1990@gmail.com>
2020-06-22 15:16:49 +0300

    Сделал 3 задачу из 3х

--- homework8/task_1_3.py
+++ homework8/task_1_3.py
@@ -9,5 +9,6 @@ data = ['12.02.2020', '01.03.2020', '31.01.2020']
 with open('text_files/task_1_3.csv', 'r', newline='') as file:
     reader = csv.reader(file)
     for row in reader:
-        print(row)
-        max_data = list(map(lambda x: x.replace('.', ' '), row)
+        true_time = list(map(lambda x: x.replace('.', ' '), row))
+        time_true_format = list(map(lambda x: datetime.strptime(x, "%d %m %Y"), true_time))
+        print(f"Минимальная дата в списке: {min(time_true_format)}")
