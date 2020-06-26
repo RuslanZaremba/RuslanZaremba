0b875df44eaae5a10dccd71b4d526ae6317f1509
RuslanZaremba <ruslanzaremba1990@gmail.com>
2020-06-26 13:45:53 +0300

    Сделал задание про библиотеку кроме сортировки.

--- homework_9/library.py
+++ homework_9/library.py
@@ -1,22 +1,33 @@
 class Library:
-    def __init__(self, name, autor, year):
-        self.name = name
-        self.autor = autor
-        self.year = year
-        self.archive = []
+    def __init__(self, **kwargs):
+        self.data = [i[1] for i in kwargs.items()]
 
     def add(self, **kwargs):
-        for i in kwargs:
-            self.archive.append({
-                'name': self.name,
-                'autor': self.autor,
-                'year': self.year
-            })
+        self.data.extend([i[1] for i in kwargs.items()])
 
-    @property
-    def get_archive(self):
-        return self.archive
-book = Library('Mtsiri', 'Pushkin', 1899)
-book.add()
-print(book.get_archive)
+    def find(self, val):
+        data_for_find = []
+        for i in self.data:
+            if val in i.values():
+                data_for_find.append(i)
+        return data_for_find
 
+    def deleter(self, value):
+        for i in self.find(value):
+            if i in self.data:
+                self.data.remove(i)
+
+    def sort(self, field):
+        return self.data.sort(field)
+
+
+book = Library(
+    first={'name': 'Mtsiri', 'autor': 'Pushkin', 'genre': 'story', 'date': 1900},
+    second={'name': 'Mumu', 'autor': 'Tolstoi', 'genre': 'story', 'date': 1905}
+)
+book.add(sec={'name': 'Mother', 'autor': 'Esenin', 'genre': 'poem', 'date': 1895})
+# print(book.data)
+# print(book.find(1900))
+# book.deleter('Mumu')
+print(book.data)
+print(book.sort(name))
